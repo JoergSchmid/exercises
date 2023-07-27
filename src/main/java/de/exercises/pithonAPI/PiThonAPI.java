f90da5a53025f221d8c72bcd25b9e71cdc8a1b3a
Joerg <Joerg.Schmid1995@gmail.com>
2023-07-27 19:33:15 +0200

    Refactor code from main() into new getDigits() method.

--- src/main/java/de/exercises/pithonAPI/PiThonAPI.java
+++ src/main/java/de/exercises/pithonAPI/PiThonAPI.java
@@ -7,11 +7,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class PiThonAPI {
+    public static final String PITHON_URL = "http://localhost:5000/api";
 
     public static void main(String[] args) {
+        System.out.println(getDigits(args[0], Integer.parseInt(args[1])));
+    }
+
+    public static String getDigits(String number, int amount) {
         try {
-            String params = "?number=" + args[0] + "&amount=" + args[1] + "&index=0";
-            URL url = new URL("http://localhost:5000/api" + params);
+            String params = "?number=" + number + "&amount=" + amount + "&index=0";
+            URL url = new URL(PITHON_URL + params);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
 
@@ -24,9 +29,9 @@ public class PiThonAPI {
                 BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 String response = in.readLine();
                 in.close();
-                System.out.println(response);
+                return response;
             } else {
-                System.out.println(con.getResponseCode());
+                return "Error: received status code" + con.getResponseCode();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
